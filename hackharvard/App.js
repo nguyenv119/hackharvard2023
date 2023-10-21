980e8b87b4ec349cc4e341f4064139f89ef22adb
Ming-Shih <minstone@gmail.com>
2023-10-21 14:47:16 -0400

    basic navigation between entry and insights

--- hackharvard/App.js
+++ hackharvard/App.js
@@ -1,61 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import NewEntry from './screens/NewEntry';
+import Insights from './screens/Insights';
 
 const Stack = createNativeStackNavigator();
 
-function MyStack() {
-	return (
-		<Stack.Navigator>
-			<Stack.Screen name="Home" component={HomeScreen} />
-			<Stack.Screen name="Details" component={DetailsScreen} />
-		</Stack.Navigator>
-	);
-}
 
 export default function App() {
 
 
-
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name="Home" component={HomeScreen} />
+			<Stack.Navigator initialRouteName='NewEntry'>
+				{/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+				<Stack.Screen name="NewEntry" component={NewEntry} />
+				<Stack.Screen name="Insights" component={Insights} />
 			</Stack.Navigator>
-			{/* <View style={styles.container}>
-				<Text style={styles.text}>Good morning, Vietnam!</Text>
-				<StatusBar style="auto" /> */}
-			{/* <View style={styles.journalContainer}>
-				<Text>Test</Text>
-			</View> */}
-			{/* </View>
-			<Button
-				title="Go to Details"
-				onPress={() => navigation.navigate('Details')}
-			/> */}
+
 		</NavigationContainer>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#EADBCB',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	journalContainer: {
-		flex: 1,
-		backgroundColor: '#EADBCB',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	text: {
-		color: '#0B2447',
-		fontSize: 20,
-		top: -300,
-		fontWeight: 'bold',
-	}
+
 });
