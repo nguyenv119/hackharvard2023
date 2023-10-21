0eb9e230d0485ce7d8ce3f08f53d5671aaa21c49
Ming-Shih <minstone@gmail.com>
2023-10-21 13:34:13 -0400

    style

--- hackharvard/App.js
+++ hackharvard/App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	const [fontsLoaded] = useFonts({
+		'Tilt-Neon': require('./assets/fonts/TiltNeon-Regular-VariableFont_XROT,YROT.ttf'),
+	});
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Good morning, Vietnam!</Text>
+			<StatusBar style="auto" />
+			<View style={styles.journalContainer}>
+				<Text>Test</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#EADBCB',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	journalContainer: {
+		flex: 1,
+		backgroundColor: '#EADBCB',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		color: '#0B2447',
+		fontSize: 20,
+		top: -300,
+		fontWeight: 'bold',
+		fontFamily: 'Tilt-Neon',
+	}
 });
